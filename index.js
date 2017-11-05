44c8164ef30b311a0382305bfceea8f74178481a
Chiranjiv-Roy <chiranjiv.roy@kofax.com>
2017-11-06 00:36:14 +0530

    File upload and transfer added

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ var app = express();
 var server = app.use(siofu.router).listen(8080);
 var io = socketio.listen(server);
 
-
-
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/download', (req, res) => {
+	var filename = req.body.file;
+	console.log(path.join(__dirname, 'upload_files/'+filename));
+  res.download(path.join(__dirname, 'upload_files/'+filename));
 });
 
 io.on('connection', function (socket) {
@@ -22,6 +26,10 @@ io.on('connection', function (socket) {
   var uploader = new siofu();
     uploader.dir = path.join(__dirname, 'upload_files');
     uploader.listen(socket);
+    uploader.on("saved", (event) => {
+    	console.log(event);
+        io.sockets.emit('file received', event.file);
+    });
   socket.on('message sent', (message) => {
   	io.sockets.emit('message received', message);
   });
--- public/js/client.js
+++ public/js/client.js
@@ -10,6 +10,26 @@ $(window).on('load',function(){
  
       $('.list-unstyled').append(html);
     };
+    
+    var addFileLi = (file) => {
+      console.log(file);
+      var source   = document.getElementById('text-file-template').innerHTML;
+      var template = Handlebars.compile(source);
+      var d = new Date(); 
+      var time = d.toISOString();
+      var context = {name: file.name, size: file.size, time: time};
+      var html    = template(context);
+ 
+      $('.list-unstyled').append(html);
+    };
+    
+    var download = (filename) => {
+      console.log("Inside download");
+      $.ajax({
+        url:'/download',
+        type:'GET',
+        data:{file: filename}});
+    }
 
     var socket = io.connect();
     $('#btn-chat').click((e) => {
@@ -17,7 +37,7 @@ $(window).on('load',function(){
       socket.emit('message sent', $('#btn-input').val());
     });
     socket.on('message received', (message) => addLi(message));
-
+    socket.on('file received', (file) => addFileLi(file));
     var uploader = new SocketIOFileUpload(socket);
     console.log(uploader);
     uploader.listenOnInput(document.getElementById("siofu_input"));
