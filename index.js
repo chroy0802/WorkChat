342207343dd23dcc3b91c765d7fd5ab3bb2cbbed
Chiranjiv-Roy <chiranjiv.roy@kofax.com>
2017-12-08 00:33:47 +0530

    session management added

--- index.js
+++ index.js
@@ -6,19 +6,44 @@ const express = require('express'),
 
 
 var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var server = app.use(siofu.router).listen(8080);
 var io = socketio.listen(server);
+var session = require('express-session');
 
+var redis = require('redis');
+const RedisStore = require('connect-redis')(session);
+var rClient = redis.createClient();
+var sessionStore = new RedisStore({client:rClient});
+
+var socketIOExpressSession = require('socket.io-express-session');
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({store:sessionStore, secret:'your secret here', resave: true, saveUninitialized: true}));
+io.use(socketIOExpressSession(app.session));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'));
+	res.sendFile(path.join(__dirname, 'public/login.html'));
 });
+
+var SessionSockets = require('session.socket.io');
+var sessionSockets = new SessionSockets(io, sessionStore, cookieParser, 'jsessionid');
 
 app.get('/download', (req, res) => {
 	var filename = req.query['file'];
 	console.log(path.join(__dirname, 'upload_files/'+filename));
   res.download(path.join(__dirname, 'upload_files/'+filename));
+});
+
+app.post('/login', function (req, res) {
+    //store user info in session after login.
+    console.log(req.body);
+    //req.session.user = req.body.user;
+    //TODO
+    //res.render('public/index');
 });
 
 io.on('connection', function (socket) {
